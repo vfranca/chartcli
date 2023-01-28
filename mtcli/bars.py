1fda581db7875804cd014206de34472972828b21
Valmir Franca <vfranca3@gmail.com>
2023-01-28 08:26:48 -0300

    2a etapa de refatoracao do modulo bars

--- mtcli/bars.py
+++ mtcli/bars.py
@@ -31,15 +31,20 @@ def bars(symbol, view, period, count, date):
     for i in range(len(list_rates)):
         dict_rates[list_rates[i][0]] = list_rates[i][1:]
     # Prepara a string de exibição no formato mínimo
+    # ASC 35.00 30.00 32.50
     view_min = "%s %.{0}f %.{1}f %.{2}f".format(d, d, d)
     # Prepara a string de exibição no formato ranges
+    # ASC 5.00
     view_ranges = "%s %.{0}f".format(d)
     # Prepara a string de exibição no formato fechamentos
+    # 32.50
     view_closes = "%.{0}f".format(d)
     # Prepara a string de exibição no formato variações percentuais
-    view_percentuais = "%.2f%%"
+    # ASC 1.50%
+    view_percentuais = "%s %.2f%%"
     # Prepara a string de exibição no formato completo
-    view_full = "%s %s %s %s %.{0}f %.{1}f %.{2}fM%.{3}f".format(d, d, d, d)
+    # ASC CP VERDE75 G2.5 BOTTOM20 35.00 30.00 32.00M32.50 5.00 2.50%
+    view_full = "%s %s %s%i %s %s %.{0}f %.{1}f %.{2}fM%.{3}f".format(d, d, d, d)
     # Exibe as barras no formato mínimo
     if view and view.lower() == "ch":
         for v in dict_rates.values():
@@ -54,16 +59,36 @@ def bars(symbol, view, period, count, date):
         return 0
     # Exibe as barras no formato completo
     for v in dict_rates.values():
+        # Define os preços de abertura, máxima, mínima e fechamento
+        o, h, l, c = float(v[0]), float(v[1]), float(v[2]), float(v[3])
+        # Calcula o range do corpo
+        range_body = c - o
+        # Define a tendência da barra
+        trend_bar = "doji"
+        if range_body > 0:
+            trend_bar = "verde"
+        if range_body < 0:
+            trend_bar = "vermelho"
+        # Calcula o range da barra
+        range_bar = h - l
+        # Calcula o percentual de corpo
+        percentual_body = abs(range_body) / range_bar * 100
+        # Verifica se é uma barra de rompimento
+        breakout = ""
+        if percentual_body >= 60:
+            breakout = "BO"
         click.echo(
             view_full
             % (
                 "asc".upper(),
-                "cp".upper(),
-                "verde90".upper(),
+                breakout.upper(),
+                trend_bar.upper(),
+                percentual_body,
                 "g200".upper(),
-                float(v[1]),
-                float(v[2]),
-                float(v[3]),
+                "BOTTOM10".upper(),
+                h,
+                l,
+                c,
                 18.34,
             )
         )
