0b8e891b67c33a8f149b25f5460e5a5929662878
Valmir Franca <vfranca3@gmail.com>
2023-02-01 03:49:35 -0300

    1a etapa de refatoracao dos tests para o modulo bars

--- tests/test_bars.py
+++ tests/test_bars.py
@@ -1,69 +1,17 @@
 # mtcli
 # Copyright 2023 Valmir França da Silva
 # http://github.com/vfranca
-from unittest import TestCase, mock, skip
 from click.testing import CliRunner
+from pytest import mark
 from mtcli.mt import mt
 
 
-class TestBars(TestCase):
-    def setUp(self):
-        self.runner = CliRunner()
+run = CliRunner()
 
-    def test_exibe_o_grafico_completo_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1"]
-        )
-        self.assertEqual(
-            res.output, " ASC  DOJI7R0.02  TOP64 18.57 18.29 18.37MP18.43 R0.28 -0.38\n"
-        )
-        self.assertEqual(res.exit_code, 0)
+def test_exibe_o_grafico_no_formato_minimo():
+    res = run.invoke(mt, ["bars", "ibov", "--view", "ch", "--count", "1"])
+    assert res.output == " 114191 112044 112316\n"
 
-    def test_exibe_o_grafico_minimo_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "ch"]
-        )
-        self.assertEqual(res.output, " ASC 18.57 18.29\n")
-        self.assertEqual(res.exit_code, 0)
+# def test_exibe_o_grafico_no_formato_ranges():
+# def test_exibe_o_grafico_no_formato_completo():
 
-    def test_exibe_o_grafico_de_fechamentos_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "c"]
-        )
-        self.assertEqual(res.output, " 18.37\n")
-        self.assertEqual(res.exit_code, 0)
-
-    def test_exibe_o_grafico_de_maximas_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "h"]
-        )
-        self.assertEqual(res.output, " 18.57\n")
-        self.assertEqual(res.exit_code, 0)
-
-    def test_exibe_o_grafico_de_minimas_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "l"]
-        )
-        self.assertEqual(res.output, " 18.29\n")
-        self.assertEqual(res.exit_code, 0)
-
-    def test_exibe_o_grafico_de_volume_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "vol"]
-        )
-        self.assertEqual(res.output, " ASC VERMELHO 16466\n")
-        self.assertEqual(res.exit_code, 0)
-
-    def test_exibe_grafico_de_ranges_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "r"]
-        )
-        self.assertEqual(res.output, " ASC VERMELHO 0.28\n")
-        self.assertEqual(res.exit_code, 0)
-
-    def test_exibe_o_grafico_de_variacao_percentual_da_ultima_barra_do_diario(self):
-        res = self.runner.invoke(
-            mt, ["bars", "abev3", "--period", "d1", "--count", "1", "--view", "var"]
-        )
-        self.assertEqual(res.output, " ASC -0.38\n")
-        self.assertEqual(res.exit_code, 0)
--- tests/test_rm.py
+++ tests/test_rm.py
@@ -9,7 +9,7 @@ from mtcli.mt import mt
 run = CliRunner()
 
 
-def test_exibe_o_range_medio_do_diario_da_abev3_de_14_periodos():
-    res = run.invoke(mt, ["rm", "abev3", "--period", "d1", "--count", "14"])
-    assert res.output == "0.34\n"
+def test_exibe_o_range_medio_do_diario():
+    res = run.invoke(mt, ["rm", "ibov"])
+    assert res.output == "1926.0\n"
     assert res.exit_code == 0
